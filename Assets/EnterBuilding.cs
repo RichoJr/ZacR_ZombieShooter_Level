00e15f91d4ecd93ccf612e6fee87a886188d8faa
RichoJr <rzac1401@gmail.com>
2023-05-04 16:41:39 +1000

    Cleaned Up Feature Asset
    Changed the feature asset from the statue to the meat hooks.

--- Assets/EnterBuilding.cs
+++ Assets/EnterBuilding.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnterBuilding : MonoBehaviour
 {
     public GameObject player;
+    public GameObject decoy;
     public Transform building;
 
     [Header("Events")]
